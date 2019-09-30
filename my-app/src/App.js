3fc847dd39006b6c95e9e9b80bf05075900315b8
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-09-30 10:09:48 +0200

    Package jSON / Also boostrap fix
    more npm installation and created a startpage view

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import StartPage from './views/StartPage'
 
 
-function App() {
-  return (
-    <div className="App">
-     <header>Hello</header>
-     <main>This is the main content</main>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {/* <Header /> */}
+          <main>
+            <Switch>
+              <Route exact path="/" component={StartPage} />
+            </Switch>
+          </main>
+          {/* <Footer /> */}
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ my-app/src/views/StartPage/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./style.scss"
+
+
+class StartPage extends React.Component {
+  render() {
+    return(
+    <div>
+    <h3 className="mb-5 mb-md-3">Startsidan</h3>
+    <h3 className="mb-5 mb-md-3">kommer utveckla massa react här!</h3>
+  </div>
+    )
+  }
+}
+
+export default StartPage
