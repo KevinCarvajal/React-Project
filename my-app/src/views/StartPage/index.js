d41bc329cff6efd29e8a8288d279e7583e1d0d66
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-12-12 20:25:55 +0100

    Startpage fixes
    Commented out some components that i won't be using yet

--- my-app/src/views/StartPage/index.js
+++ my-app/src/views/StartPage/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Col, Row, Container } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const StartPage = () => {
   return (
     <Container fluid className="startpage">
-      <h2 className="page-title">Startsidan</h2>
-
+      <h2 className="page-title">Komponenter</h2>
       <Row className="text-center">
         <Col lg={4}>
-          <h3>FormPage</h3>
+          <Link to="/FormPage">
+            <h3>FormPage</h3>
+          </Link>
         </Col>
         <Col lg={4}>
           <h3>Info</h3>
